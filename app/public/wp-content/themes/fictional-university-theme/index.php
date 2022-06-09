7a83e54cab436fcee20f99e2533d4c377c00614d
WD40 <69653174+duncangw1@users.noreply.github.com>
2022-06-09 09:55:30 -0400

    Adding Prettier php plugin to format code

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,22 +1,9 @@
-<?php 
+<?php
 
-$names = array('Will', 'Bill', 'Frank', 'Julie');
-
-$count = 0;
-
-while($count < count($names)) {
-    echo "<li>Hi, my name is $names[$count]</li>";
-    $count++;
+while (have_posts()) {
+  the_post(); ?>
+<h2>Hello</h2>
+  <?php
 }
-
-function greet($name, $color) {
-    echo "<p>Hi, my name is $name and my favorite color is $color</p>";
-}
-
-greet('John', 'blue');
-greet('Jane', 'green');
 
 ?>
-
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
